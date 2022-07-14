ce5bdc693507339999f63ce7cf3365b7d9139379
David Cañas <dcanasp@unal.edu.com>
2022-07-14 15:39:43 -0500

    inicio nada

---
+++ back/coneccion.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    //res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Type', 'text/html');
+    const url = req.url;
+    const method = req.method;
+    let bodyFinal    
+    const body = [];//array
+    req.on('data', (chunk) => {
+
+        //console.log(chunk);//da como resultsdo algo ilegible
+        body.push(chunk);//se puede hacer push asi sea cnt
+        console.log(req.params('name'));
+    })
+    req.on('end', () => {
+       bodyFinal = Buffer.concat(body).toString();//pasa chunk a algo legible
+       console.log(bodyFinal);
+      });
+    req.on("end", () => {
+        res.write("ojala")
+ 
+        res.end();  
+    })
+
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
